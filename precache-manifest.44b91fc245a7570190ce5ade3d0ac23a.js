c87cc50d9024afc6bcda28a6b8fd20d315ba5f92
Ben Roth <spongebucket@gmail.com>
2019-06-16 20:27:12 -0700

    Updates

---
+++ precache-manifest.44b91fc245a7570190ce5ade3d0ac23a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d56a4ca126b41a646283a169f9892ec",
+    "url": "/bitgraves-www/index.html"
+  },
+  {
+    "revision": "40b465389b7b5d08eb24",
+    "url": "/bitgraves-www/static/css/main.4a5a5004.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/bitgraves-www/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "40b465389b7b5d08eb24",
+    "url": "/bitgraves-www/static/js/main.bcc09c87.chunk.js"
+  },
+  {
+    "revision": "e4fce9aa9c1d4acd7ff9",
+    "url": "/bitgraves-www/static/js/runtime~main.27379c15.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bitgraves-www/precache-manifest.dd6636609794f658268eed96be0a6db6.js"
+  "/bitgraves-www/precache-manifest.44b91fc245a7570190ce5ade3d0ac23a.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/bitgraves-www/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bitgraves-www/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
