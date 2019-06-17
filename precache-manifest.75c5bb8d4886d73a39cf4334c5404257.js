4b596cdbcc49f1905b0de2c45b3e067bb47a0309
Ben Roth <spongebucket@gmail.com>
2019-06-16 20:46:18 -0700

    Updates

---
+++ precache-manifest.75c5bb8d4886d73a39cf4334c5404257.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c1fd611c44333d82dcea4b3b3af3a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c73f447b3ac2ee1ffe9f",
+    "url": "/static/css/main.4a5a5004.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c73f447b3ac2ee1ffe9f",
+    "url": "/static/js/main.51cb9c9f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bitgraves-www/precache-manifest.44b91fc245a7570190ce5ade3d0ac23a.js"
+  "/precache-manifest.75c5bb8d4886d73a39cf4334c5404257.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bitgraves-www/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
