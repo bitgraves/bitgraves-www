95b7475330e80c480f4a6217330e89555c2ce8a1
Ben Roth <spongebucket@gmail.com>
2019-06-16 20:11:29 -0700

    Updates

---
+++ precache-manifest.dd6636609794f658268eed96be0a6db6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4fce9aa9c1d4acd7ff9",
+    "url": "/bitgraves-www/static/js/runtime~main.27379c15.js"
+  },
+  {
+    "revision": "ebaa32e1b2f091223d27",
+    "url": "/bitgraves-www/static/js/main.bcc09c87.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/bitgraves-www/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ebaa32e1b2f091223d27",
+    "url": "/bitgraves-www/static/css/main.b2b4d57b.chunk.css"
+  },
+  {
+    "revision": "d91f952c3aa5ab0eb5e569912531fc21",
+    "url": "/bitgraves-www/index.html"
+  }
+];
