84ea5100e16fb8f8a05862f26cadf5cc74b8abef
Ben Roth <spongebucket@gmail.com>
2020-06-27 13:57:49 -0700

    Updates

---
+++ precache-manifest.c25938069b56dbc36d0b4c9648aac2e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb2fa8cb7e112d59009bd210cbaa90c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1af821fa3903bc8ea688",
+    "url": "/static/css/main.6912492a.chunk.css"
+  },
+  {
+    "revision": "8f2f31625a99e09acfa0",
+    "url": "/static/js/2.ae6bf41f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ae6bf41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af821fa3903bc8ea688",
+    "url": "/static/js/main.683f1594.chunk.js"
+  },
+  {
+    "revision": "7aea227f6877cce0d16d",
+    "url": "/static/js/runtime-main.900e1ccd.js"
+  },
+  {
+    "revision": "ac3d6f412e61032f79250d412d65d25c",
+    "url": "/static/media/social-preview.ac3d6f41.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5aeb7b97254355e90a8cb8cf791ef2b3.js"
+  "/precache-manifest.c25938069b56dbc36d0b4c9648aac2e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
