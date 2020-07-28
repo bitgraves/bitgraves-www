421d5f278c65d1395527650bd3f08502823da7c3
Ben Roth <spongebucket@gmail.com>
2020-07-28 15:19:34 -0700

    Updates

---
+++ precache-manifest.7556c4bca0989b20b768e48d65639a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ecdd630ed4cced2d4ed152342a4d7c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c192d8bcbba049cac245",
+    "url": "/static/css/main.3762de1b.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "c192d8bcbba049cac245",
+    "url": "/static/js/main.0d4e3319.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ac3d6f412e61032f79250d412d65d25c",
+    "url": "/static/media/social-preview.ac3d6f41.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04aec329acd6034404d0aa3fba97d964.js"
+  "/precache-manifest.7556c4bca0989b20b768e48d65639a62.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
